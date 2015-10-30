9630f16a8e3ce694b22795f7f3a1ebef6a733544
Tony Kieu <tkieu87@gmail.com>
2015-10-30 16:15:58 -0400

    Initial CheckIn

---
+++ ECBToolkit/ECBToolkit.h
@@ -0,0 +1,19 @@
+//
+//  ECBToolkit.h
+//  ECBToolkit
+//
+//  Created by Tony Kieu on 10/30/15.
+//  Copyright © 2015 ECOMBID, INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ECBToolkit.
+FOUNDATION_EXPORT double ECBToolkitVersionNumber;
+
+//! Project version string for ECBToolkit.
+FOUNDATION_EXPORT const unsigned char ECBToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ECBToolkit/PublicHeader.h>
+
+
